f2f63c8ea2d58fad3e1983543e5503bc95a166fc
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-05-17 22:07:54 -0600

    placeholder text updated

--- src/Components/NavBar/Input.jsx
+++ src/Components/NavBar/Input.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react'
 
 const Input = () => {
   return (
     <input
-      className="form-control me-2"
-      type="search"
-      placeholder="Search"
-      aria-label="Search"
+      className='form-control me-2'
+      type='search'
+      placeholder='Search planets, stars maybe aliens...?'
+      aria-label='Search'
     />
-  );
-};
+  )
+}
 
-export default Input;
+export default Input
