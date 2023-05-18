b504a0aa3cc36eb3450b2acea149693d846de6b9
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-05-17 22:04:18 -0600

    browserRouter added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import Home from "./Components/Home";
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+import Home from './Components/Home'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>
+  </BrowserRouter>
+)
