65a0319a7db9755a5ca8af0b02c3371b9b6661a8
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-05-17 22:06:39 -0600

    routes and route added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,21 @@
-import Card from "./Card";
-import NavBar from "./NavBar/NavBar";
-import Carousel from "./carousel/Carousel";
+import Card from './Card'
+import NavBar from './NavBar/NavBar'
+import Carousel from './carousel/Carousel'
+import { Routes, Route } from 'react-router-dom'
+import DayImg from './dayImage/DayImg'
 
 function Home() {
   return (
     <>
       <NavBar />
-      <Carousel />
-      <Card />
+      <DayImg />
+
+      <Routes>
+        <Route path='/' element={<Card />} />
+        <Route path='/Carousel' element={<Carousel />} />
+      </Routes>
     </>
-  );
+  )
 }
 
-export default Home;
+export default Home
