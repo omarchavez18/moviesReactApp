172198253df9140ebd1251d119912aa48d558314
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-05-27 19:26:43 -0600

    made the "card" component dynamic

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,25 +1,15 @@
 import React from 'react'
 import Image from './carousel/Image'
-import Hyperlink from './NavBar/Hyperlink'
 
-const Card = () => {
+const Card = ({ clasName, src, title, text, children }) => {
   return (
     <>
       <div className='card m-4' style={{ width: '18rem' }}>
-        <Image
-          src='https://viajes.nationalgeographic.com.es/medio/2018/10/30/cho-oyu_e35b898f_1500x907.jpg'
-          className='card-img-top'
-          alt=''
-        />
+        <Image src={src} className={clasName} alt='' />
         <div className='card-body'>
-          <h5 className='card-title'>Card title</h5>
-          <p className='card-text'>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <Hyperlink href='' className='btn btn-primary mt-2'>
-            star
-          </Hyperlink>
+          <h5 className='card-title'>{title}</h5>
+          <p className='card-text'>{text}</p>
+          <p>{children}</p>
         </div>
       </div>
     </>
