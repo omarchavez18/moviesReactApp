2d4996e55154452f97cea7cd3db7599fb872686f
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-05-27 19:30:29 -0600

    routes added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,15 +4,18 @@ import Carousel from './carousel/Carousel'
 import { Routes, Route } from 'react-router-dom'
 import DayImg from './dayImage/DayImg'
 import Login from './login/Login'
+import Cards from './cards/Cards'
 
 function Home() {
   return (
     <>
       <NavBar />
-      <DayImg />
+      <Cards />
 
       <Routes>
-        <Route path='/' element={<Card />} />
+        {/* <Route path='/' element={<DayImg />} /> */}
+        <Route path='/DayImage' element={<DayImg />} />
+        <Route path='/Card' element={<Card />} />
         <Route path='/Carousel' element={<Carousel />} />
         <Route path='/Login' element={<Login />} />
       </Routes>
