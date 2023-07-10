9c2499ee87fc5e7653adf7ac59d5459045d09970
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-07-09 22:56:35 -0600

    component card remove and marsRoverPhotos added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,23 +1,20 @@
-import Card from './Card'
 import NavBar from './NavBar/NavBar'
 import Carousel from './carousel/Carousel'
 import { Routes, Route } from 'react-router-dom'
 import DayImg from './dayImage/DayImg'
-import Login from './login/Login'
-import Cards from './cards/Cards'
+import Login from './loginWithLocalStorage/Login'
+import Mars from './mars/Mars'
 
 function Home() {
   return (
     <>
       <NavBar />
-      <Cards />
 
       <Routes>
-        {/* <Route path='/' element={<DayImg />} /> */}
         <Route path='/DayImage' element={<DayImg />} />
-        <Route path='/Card' element={<Card />} />
+        <Route path='/MarsRoverPhotos' element={<Mars />} />
         <Route path='/Carousel' element={<Carousel />} />
-        <Route path='/Login' element={<Login />} />
+        <Route path='/Login' element={<Login />} />{' '}
       </Routes>
     </>
   )
