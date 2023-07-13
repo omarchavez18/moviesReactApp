eca0379454dd2b0f8219ff5eb06a5abfb184ab89
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-07-12 23:38:25 -0600

    added function to show title, description and  img on the component, and change by every click of the btn

--- src/Components/carousel/Carousel.jsx
+++ src/Components/carousel/Carousel.jsx
@@ -1,61 +1,89 @@
-import React from "react";
-import Image from "./Image";
-import Button from "../NavBar/Button";
+import React, { useState } from 'react'
+import Image from './Image'
+import Button from '../NavBar/Button'
 
-function Carousel() {
+function Carousel({
+  slides,
+  classNameCarouselBody,
+  classNameImg,
+  sectionTitleDescription,
+}) {
+  const [actualSlide, setActualSlide] = useState(0)
+
+  function nextSlide() {
+    setActualSlide(actualSlide + 1)
+  }
+  function previousSlide() {
+    setActualSlide(actualSlide - 1)
+  }
   return (
     <>
-      <div id="carouselExampleFade" className="carousel slide carousel-fade">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <Image
-              src="https://media.istockphoto.com/id/1293741276/photo/majestic-view-of-the-k2-peak.jpg?b=1&s=170667a&w=0&k=20&c=Zls4snzMqeupClx7hQRIqbBanjnEUO_kw4Cf8e7OM2o="
-              className="d-block w-100"
-              alt="K2"
-            />
-          </div>
-          <div className="carousel-item">
-            <Image
-              src="https://c4.wallpaperflare.com/wallpaper/564/174/495/himalaya-nepal-asia-pokhara-wallpaper-preview.jpg"
-              className="d-block w-100"
-              alt="Annapurna"
-            />
-          </div>
-          <div className="carousel-item">
-            <Image
-              src="https://viajes.nationalgeographic.com.es/medio/2018/10/30/cho-oyu_e35b898f_1500x907.jpg"
-              className="d-block w-100"
-              alt="Everest"
-            />
-          </div>
+      <div
+        id='carouselExampleIndicators'
+        className={`carousel slide ${classNameCarouselBody}`}
+      >
+        <div className='carousel-inner'>
+          {slides.map((slide, i) => {
+            return (
+              <div
+                key={i}
+                className={`carousel-item  ${slide === slides[0] && 'active'}`}
+              >
+                <Image src={slide?.href} className={classNameImg} alt='...' />
+              </div>
+            )
+          })}
         </div>
+
         <Button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="prev"
+          onClick={previousSlide}
+          className='carousel-control-prev'
+          type='button'
+          data-bs-target='#carouselExampleIndicators'
+          data-bs-slide='prev'
         >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
+          <span className='carousel-control-prev-icon' aria-hidden='true' />
+          <span className='visually-hidden'>Previous</span>
         </Button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="next"
+
+        <Button
+          onClick={nextSlide}
+          className='carousel-control-next'
+          type='button'
+          data-bs-target='#carouselExampleIndicators'
+          data-bs-slide='next'
         >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+          <span className='carousel-control-next-icon' aria-hidden='true' />
+          <span className='visually-hidden'>Next</span>
+        </Button>
       </div>
+
+      <section className={sectionTitleDescription}>
+        {/* description */}
+        {slides ? (
+          <div>
+            <p>{slides[actualSlide]?.description}</p>
+          </div>
+        ) : (
+          ''
+        )}
+
+        {/* title */}
+        {slides ? (
+          <div>
+            <p>{slides[actualSlide]?.title}</p>
+          </div>
+        ) : (
+          ''
+        )}
+      </section>
     </>
-  );
+  )
 }
 
-export default Carousel;
+export default Carousel
+
+/* cosas por hacer:
+el boton previous va para atras pero si dan atras desde la primer imagen,
+ si cambia la imagen pero el texto no actualiza
+*/
