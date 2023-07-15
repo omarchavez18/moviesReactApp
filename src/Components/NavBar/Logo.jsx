223cbccf6c25475a8647288fff8fde546f0124a4
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-07-14 21:28:13 -0600

    href pass via props

--- src/Components/NavBar/Logo.jsx
+++ src/Components/NavBar/Logo.jsx
@@ -3,8 +3,8 @@ import Image from '../carousel/Image'
 import styles from '../../Styles/NavBar/logo.module.scss'
 import logo from '../../images/nasaLogo.png'
 
-const Logo = () => {
-  return <Image src={logo} className={styles.logo} />
+const Logo = ({ href }) => {
+  return <Image href={href} src={logo} className={styles.logo} />
 }
 
 export default Logo
