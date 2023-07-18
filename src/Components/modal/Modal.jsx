b013248cc16d3194fb1a75ae11dcb6b28c80d95e
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-07-18 16:31:32 -0600

    this function works with home component to close the modal after press "x"

--- src/Components/modal/Modal.jsx
+++ src/Components/modal/Modal.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react'
 import Image from '../carousel/Image'
 import styles from './../../Styles/modal/modal.module.scss'
 
-const Modal = ({ selectedImage }) => {
+const Modal = ({ selectedImage, onStateChange }) => {
+  const [value, setValue] = useState()
+
+  function handleChange() {
+    setValue(false)
+    onStateChange(value)
+  }
+
   return (
     <>
       {
         <div className={styles.overlay}>
           <Image src={selectedImage} className={styles.img} />
-          <p>X</p>
+          <p onClick={handleChange}>X</p>
         </div>
       }
     </>
