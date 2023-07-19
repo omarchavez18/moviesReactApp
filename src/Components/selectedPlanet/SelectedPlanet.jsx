e8edb618224bbf83d44477226a2565c83b76073d
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-07-18 22:19:32 -0600

    notes added

--- src/Components/selectedPlanet/SelectedPlanet.jsx
+++ src/Components/selectedPlanet/SelectedPlanet.jsx
@@ -5,3 +5,12 @@ const SelectedPlanet = () => {
 }
 
 export default SelectedPlanet
+
+/*
+ 2)como pasar la variable allPlanets de componente planets a componente selected planet,
+ 4) como cambiar color a navbar de la seleccion en la que este,
+ 5) url en selectedPlanets no debe tener espacios,
+ 6) en mars rovers si no hay informacion a devolver que salga un mensaje que diga que intente otra fecha u otra camara,
+ 7) en componente planet en el buscador si busca informacion pero no hay que diga que intente con otra palabra.
+
+*/
