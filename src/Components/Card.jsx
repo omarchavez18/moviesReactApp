819bad5c7cac713001b9be0a8d3c35aa5549a300
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-07-19 23:00:21 -0600

    add onCardClick to recieve a function when I click on

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Image from './carousel/Image'
 
-const Card = ({ classNameImg, className, src, title, text, children }) => {
+const Card = ({
+  classNameImg,
+  className,
+  src,
+  title,
+  text,
+  children,
+  onCardClick,
+}) => {
   return (
     <>
-      <div className={` ${className} card`}>
+      <div className={` ${className} card`} onClick={() => onCardClick(title)}>
         <Image src={src} className={classNameImg} alt='' />
         <div className='card-body'>
           <h5 className='card-title'>{title}</h5>
