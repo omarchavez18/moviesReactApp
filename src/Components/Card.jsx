e1b0bf294b7adab08d37a8049ca07780a62c483d
Omar <omarchavezgutierrezdevelasco@gmail.com>
2023-09-11 20:15:52 -0600

    function to allow click on each image

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,9 +10,13 @@ const Card = ({
   children,
   onCardClick,
 }) => {
+  function handleClick() {
+    title && onCardClick && onCardClick(title)
+  }
+
   return (
     <>
-      <div className={` ${className} card`} onClick={() => onCardClick(title)}>
+      <div className={` ${className} card`} onClick={handleClick}>
         <Image src={src} className={classNameImg} alt='' />
         <div className='card-body'>
           <h5 className='card-title'>{title}</h5>
